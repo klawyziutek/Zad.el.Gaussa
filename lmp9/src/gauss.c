95691887683175abed70b5bf3a1c7be16fed6d66
klawyziutek <01206765@pw.edu.pl>
2025-12-19 00:58:34 +0100

    Sprawdzenie wymiarów macierzy i wektora

--- lmp9/src/gauss.c
+++ lmp9/src/gauss.c
@@ -5,10 +5,18 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+	int i, j, k;
 
-		return 0;
+	/* Sprawdzenie czy macierz A jest kwadratowa i czy wektor b i 
+	 * macierz A mają tyle samo wierszy */
+
+	if (mat->r != mat->c || mat->r != b->r){
+		return 2;
+	}
+
+	/* reszta */
+
+
+	return 0;
 }
 
