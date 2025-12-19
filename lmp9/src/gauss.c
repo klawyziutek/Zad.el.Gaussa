93b09e01ba5f1755042665e1310caae9ccb35e78
klawyziutek <01206765@pw.edu.pl>
2025-12-19 01:11:17 +0100

    Implementacja schodkowania - głównego elementu el. Gaussa

--- lmp9/src/gauss.c
+++ lmp9/src/gauss.c
@@ -14,7 +14,26 @@ int eliminate(Matrix *mat, Matrix *b){
 		return 2;
 	}
 
-	/* reszta */
+	int n = mat->r; //liczba wierszy 
+	
+	/* Pętla po skosie */
+	for(k = 0; k < n - 1; k++){
+
+		/* Pętla schodkowania */
+		for(i = k + 1; i < n; i++){
+		
+			/* Współczynnik do zerowania */
+			double factor = mat->data[i][k] / mat->data[k][k];
+
+			/* Odejmujemy wiersz k od wiersza i */
+			for(j = k; j < n; j++){
+				mat->data[i][j] -= factor * mat->data[k][j];
+			}
+
+			/*Zmiany w wektorze b */
+			b->data[i][0] -= factor * b->data[k][0];
+		}
+	}
 
 
 	return 0;
