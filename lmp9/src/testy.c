3c6b6884c2684d4af58fbc4600f659b0d683559b
klawyziutek <01206765@pw.edu.pl>
2026-01-08 23:43:08 +0100

    Dodanie funkcji przeprowadzajacej test (wykorzystywana w main)

--- lmp9/src/testy.c
+++ lmp9/src/testy.c
@@ -4,6 +4,71 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+
+/* Faktyczna funkcja testująca */
+
+void run_test(char *test_name, char *file_A, char *file_b, char *file_expected_x, int expected_exit_code) {
+    printf("Uruchamiam %s... ", test_name);
+    
+    Matrix *A = readFromFile(file_A);
+    Matrix *b = readFromFile(file_b);
+    Matrix *x;
+    Matrix *expected_x = NULL;
+    int res = 0;
+
+    if (A == NULL || b == NULL) {
+        if (expected_exit_code == -1) printf("OK (Poprawnie wykryto błąd pliku)\n");
+        else printf("BŁĄD (Nie udało się wczytać plików wejściowych)\n");
+        
+        if (A) freeMatrix(A);
+        if (b) freeMatrix(b);
+        return;
+    }
+
+    res = eliminate(A, b);
+
+    if (res != 0) {
+        if (res == expected_exit_code) {
+            printf("OK (Oczekiwany kod błędu eliminacji: %d)\n", res);
+        } else {
+            printf("BŁĄD (Eliminacja zwróciła %d, oczekiwano %d)\n", res, expected_exit_code);
+        }
+    } else {
+        x = createMatrix(b->r, 1);
+        if (x != NULL) {
+            res = backsubst(x, A, b);
+            
+            if (res == expected_exit_code) {
+                // JEŚLI MAMY PLIK WZORCOWY - SPRAWDZAMY WYNIKI
+                if (file_expected_x != NULL) {
+                    expected_x = readFromFile(file_expected_x);
+                    if (expected_x != NULL) {
+                        if (compare_matrices(x, expected_x)) {
+                            printf("OK (Wynik liczbowy poprawny)\n");
+                        } else {
+                            printf("BŁĄD (Niepoprawny wynik liczbowy)\n");
+                        }
+                        freeMatrix(expected_x);
+                    } else {
+                        printf("OSTRZEŻENIE: Nie można wczytać pliku wzorcowego %s\n", file_expected_x);
+                    }
+                } else {
+                     printf("OK (Kod powrotu poprawny, brak wzorca do sprawdzenia liczb)\n");
+                }
+            } else {
+                 printf("BŁĄD (Backsubst zwrócił %d, oczekiwano %d)\n", res, expected_exit_code);
+            }
+            freeMatrix(x);
+        } else {
+            printf("BŁĄD (Nie udało się utworzyć wektora x)\n");
+        }
+    }
+
+    freeMatrix(A);
+    freeMatrix(b);
+}
+
+
 int main() {
     printf("=== ROZPOCZYNAM AUTOMATYCZNE TESTY ===\n");
 
